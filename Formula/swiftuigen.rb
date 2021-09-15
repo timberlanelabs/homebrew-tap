f192140608fafec9092cd6af5304d734abfd7c1b
Ian Keen <iankeen82@gmail.com>
2021-09-15 14:21:41 -0700

    Create swiftuigen.rb

---
+++ Formula/swiftuigen.rb
@@ -0,0 +1,10 @@
+class SwiftUIGen < Formula
+  desc "A utility to generate helper code for SwiftUI"
+  homepage "https://github.com/timberlanelabs/SwiftUIGen"
+  url "https://github.com/timberlanelabs/SwiftUIGen/releases/download/1.0.0/swiftuigen.zip"
+  version "1.0.0"
+
+  def install
+    bin.install "swiftuigen"
+  end
+end
