f9deadc283c6aa352e77e12556fca9630996be73
Ian Keen <iankeen82@gmail.com>
2021-09-15 14:24:00 -0700

    Update swiftuigen.rb

--- Formula/swiftuigen.rb
+++ Formula/swiftuigen.rb
@@ -1,4 +1,4 @@
-class swiftuigen < Formula
+class Swiftuigen < Formula
   desc "A utility to generate helper code for SwiftUI"
   homepage "https://github.com/timberlanelabs/SwiftUIGen"
   url "https://github.com/timberlanelabs/SwiftUIGen/releases/download/1.0.0/swiftuigen.zip"
